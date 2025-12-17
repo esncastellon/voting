e6b309bea47fad25be16b834ab79ab19f4e381ac
monica <mpitarch@eriaconsulting.com>
2025-12-17 15:00:26 +0100

    feat: add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,24 @@
+import { Inter, Lusitana, Lato } from "next/font/google";
+import localFont from "next/font/local";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lato = Lato({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const kensonSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Kelson Sans Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Kelson Sans Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
